cea23f9084a00354b4a1a5b3bf271fcddffb1632
Adam Hellberg <sharparam@sharparam.com>
2014-03-27 14:16:21 +0100

    Add an RNG helper.

---
+++ src/main/java/com/sharparam/sharpmemory/helpers/RandomHelper.java
@@ -0,0 +1,12 @@
+package com.sharparam.sharpmemory.helpers;
+
+import java.util.Random;
+
+/**
+ * Created by on 2014-03-27.
+ *
+ * @author Sharparam
+ */
+public class RandomHelper {
+    public static final Random RNG = new Random();
+}
