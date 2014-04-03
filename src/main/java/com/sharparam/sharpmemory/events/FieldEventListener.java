dd8089866ba08ac14d980daf34d891c918523832
Adam Hellberg <sharparam@sharparam.com>
2014-04-03 09:34:09 +0200

    Documented and cleaned up FieldEventListener.

--- src/main/java/com/sharparam/sharpmemory/events/FieldEventListener.java
+++ src/main/java/com/sharparam/sharpmemory/events/FieldEventListener.java
@@ -1,10 +1,16 @@
 package com.sharparam.sharpmemory.events;
 
 /**
- * Created by on 2014-03-27.
+ * Event interface for FieldModel events.
+ *
+ * Created on 2014-03-27.
  *
  * @author Sharparam
  */
 public interface FieldEventListener {
+    /**
+     * Handles an event from FieldModel.
+     * @param args The type of event.
+     */
     void handle(FieldEventType args);
 }
