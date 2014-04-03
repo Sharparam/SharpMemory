91cbce64c1e5527667ef77d4a674da4019764536
Adam Hellberg <sharparam@sharparam.com>
2014-04-03 09:39:17 +0200

    Documented and cleaned up BrickHelper.

--- src/main/java/com/sharparam/sharpmemory/helpers/BrickHelper.java
+++ src/main/java/com/sharparam/sharpmemory/helpers/BrickHelper.java
@@ -6,7 +6,9 @@ import javafx.scene.image.ImageView;
 import java.util.HashMap;
 
 /**
- * Created by on 2014-03-27.
+ * Contains various helper methods related to bricks.
+ *
+ * Created on 2014-03-27.
  *
  * @author Sharparam
  */
@@ -22,12 +24,37 @@ public final class BrickHelper {
      */
     private static final HashMap<String, Image> IMAGES = new HashMap<String, Image>();
 
-    public static Image getImage(String source) {
+    /**
+     * Gets an image from the specified source.
+     * Will get image from cache if it has been previously loaded
+     * and forceNew param is false.
+     * @param source Source to load image from.
+     * @param forceNew Forces the image to be regenerated from source instead of fetched from cache.
+     * @return Image object created from the specified source.
+     */
+    public static Image getImage(String source, boolean forceNew) {
+        if (forceNew && IMAGES.containsKey(source))
+            IMAGES.remove(source);
         if (!IMAGES.containsKey(source))
             IMAGES.put(source, BrickHelper.createImage(source));
         return IMAGES.get(source);
     }
 
+    /**
+     * Gets an image object from the specified source.
+     * If the image has been previously loaded it will be fetched from cache.
+     * @param source Source to load image from.
+     * @return Image object created from the specified source.
+     */
+    public static Image getImage(String source) {
+        return getImage(source, false);
+    }
+
+    /**
+     * Creates an ImageView object displaying the specified Image.
+     * @param image Image to display.
+     * @return ImageView object.
+     */
     public static ImageView createImageView(Image image) {
         ImageView view = new ImageView(image);
         view.setFitWidth(IMAGE_SIZE);
@@ -38,10 +65,20 @@ public final class BrickHelper {
         return view;
     }
 
+    /**
+     * Creates an ImageView with an image from the specified source.
+     * @param source Source to load image from.
+     * @return ImageView object.
+     */
     public static ImageView createImageView(String source) {
         return createImageView(getImage(source));
     }
 
+    /**
+     * Creates an image from the specified source.
+     * @param source Source to create image from.
+     * @return Image object created from the specified source.
+     */
     private static Image createImage(String source) {
         return new Image(source, IMAGE_SIZE, IMAGE_SIZE, true, true, true);
     }
