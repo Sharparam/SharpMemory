0270ef291b88c909be3f931c12d24c124c3cbf4e
Adam Hellberg <sharparam@sharparam.com>
2014-04-03 09:33:26 +0200

    Documented and cleaned up FieldEventType.

--- src/main/java/com/sharparam/sharpmemory/events/FieldEventType.java
+++ src/main/java/com/sharparam/sharpmemory/events/FieldEventType.java
@@ -1,13 +1,32 @@
 package com.sharparam.sharpmemory.events;
 
 /**
- * Created by on 2014-03-27.
+ * Available field events.
+ *
+ * Created on 2014-03-27.
  *
  * @author Sharparam
  */
 public enum FieldEventType {
+    /**
+     * Two bricks were flipped.
+     * This event occurs on fails as well as clears.
+     */
     TRY,
+
+    /**
+     * Two bricks were flipped that were matches
+     * and have now been cleared.
+     */
     CLEAR,
+
+    /**
+     * Two bricks were flipped that were not matches.
+     */
     FAIL,
+
+    /**
+     * All bricks on the playing field have been cleared.
+     */
     ALL_BRICKS_CLEARED
 }
