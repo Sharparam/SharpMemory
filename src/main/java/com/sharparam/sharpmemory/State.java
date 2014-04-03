ccc0c031f33efbcf773bc946422e0f9465d49b85
Adam Hellberg <sharparam@sharparam.com>
2014-04-03 09:41:16 +0200

    Documented and cleaned up State.

--- src/main/java/com/sharparam/sharpmemory/State.java
+++ src/main/java/com/sharparam/sharpmemory/State.java
@@ -1,13 +1,30 @@
 package com.sharparam.sharpmemory;
 
 /**
- * Created by on 2014-03-27.
+ * Brick states.
+ *
+ * Created on 2014-03-27.
  *
  * @author Sharparam
  */
 public enum State {
+    /**
+     * Brick is faced down, hiding the match image.
+     */
     FACE_DOWN,
+
+    /**
+     * Brick is faced up, showing the match image.
+     */
     FACE_UP,
+
+    /**
+     * Brick is cleared.
+     */
     CLEARED,
+
+    /**
+     * Brick is invalid.
+     */
     INVALID
 }
