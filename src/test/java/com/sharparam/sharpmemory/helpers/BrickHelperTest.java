2014e8ef8ae2e1ff641dd5a2192927fa208f7720
Adam Hellberg <sharparam@sharparam.com>
2014-04-04 13:28:10 +0200

    Add file for BrickHelper tests.

---
+++ src/test/java/com/sharparam/sharpmemory/helpers/BrickHelperTest.java
@@ -0,0 +1,20 @@
+package com.sharparam.sharpmemory.helpers;
+
+import org.junit.Test;
+
+/**
+ * Created on 2014-04-04.
+ *
+ * @author Sharparam
+ */
+public class BrickHelperTest {
+    @Test
+    public void testGetImage() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateImageView() throws Exception {
+
+    }
+}
