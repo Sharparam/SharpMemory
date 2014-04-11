d92010a2b4fdba91a605130f40f3a4a6f361c6da
Adam Hellberg <sharparam@sharparam.com>
2014-04-11 10:39:38 +0200

    Implement tests for BrickHelper

--- src/test/java/com/sharparam/sharpmemory/helpers/BrickHelperTest.java
+++ src/test/java/com/sharparam/sharpmemory/helpers/BrickHelperTest.java
@@ -1,5 +1,10 @@
 package com.sharparam.sharpmemory.helpers;
 
+import com.sharparam.sharpmemory.tests.rules.JavaFXThreadingRule;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -13,13 +18,31 @@ import org.junit.runners.JUnit4;
  */
 @RunWith(JUnit4.class)
 public class BrickHelperTest {
+    @Rule
+    public JavaFXThreadingRule javaFXThreadingRule = new JavaFXThreadingRule();
+
+    private static final String TEST_IMAGE_SOURCE = "http://placehold.it/1x1";
+
     @Test
     public void testGetImage() throws Exception {
-
+        Image firstImage = BrickHelper.getImage(TEST_IMAGE_SOURCE);
+        Image secondImage = BrickHelper.getImage(TEST_IMAGE_SOURCE);
+        Assert.assertEquals("BrickHelper should return cached image on second request", firstImage, secondImage);
+        Image forced = BrickHelper.getImage(TEST_IMAGE_SOURCE, true);
+        Assert.assertNotSame("BrickHelper should return new image when forced=true", firstImage, forced);
     }
 
     @Test
     public void testCreateImageView() throws Exception {
+        Image image = BrickHelper.getImage(TEST_IMAGE_SOURCE);
+        Image forced = BrickHelper.getImage(TEST_IMAGE_SOURCE, true);
 
+        ImageView view = BrickHelper.createImageView(image);
+        Assert.assertEquals("ImageView requested with image object should have that image", image, view.getImage());
+
+        ImageView forcedView = BrickHelper.createImageView(TEST_IMAGE_SOURCE);
+        Assert.assertEquals("ImageView requested with source should have latest cached image", forced, forcedView.getImage());
+
+        Assert.assertNotSame("ImageViews with different Image objects should not be equal", view, forcedView);
     }
 }
